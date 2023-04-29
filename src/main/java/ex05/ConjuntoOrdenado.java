8f1d06b7753ce9b4805d8bf8508193dd5888eb87
HugoRibeiro-estudante <hugo.alves@estudante.iftm.edu.br>
2023-04-28 21:55:54 -0300

    Ajustado tipo generico para Comparable e adc metodo sort

--- src/main/java/ex05/ConjuntoOrdenado.java
+++ src/main/java/ex05/ConjuntoOrdenado.java
@@ -1,14 +1,17 @@
 package ex05;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
-public class ConjuntoOrdenado <T extends Number>{
+public class ConjuntoOrdenado <T extends Comparable<T>>{
 
     List<T> conj = new ArrayList<>();
 
     public void adicionar(T element){
         conj.add(element);
+        Collections.sort(conj);
     }
 
     public void remover(T element){
